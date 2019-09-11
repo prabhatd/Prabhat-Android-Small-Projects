450e1c6a6521a7fdf1e219b39707fcb31086172e
Prabhat Dwivedi <prabhatd@pune-com-dt-014.payso.in>
2019-09-11 17:55:33 +0530

    These activities Comprises of Sharing , opening map,Opening Google Website

---
+++ Day3.2/app/src/main/java/com/prabhat/mswipecardsale/implicitintent/MainActivity.java
@@ -0,0 +1,77 @@
+package com.prabhat.mswipecardsale.implicitintent;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v4.app.ShareCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText editWebsite,editlocation,editShare;
+    Button  btnWebsite,btnLocation,btnShare;
+    public static final String TAG=MainActivity.class.getSimpleName();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        editWebsite=findViewById(R.id.website);
+        editlocation= findViewById(R.id.location);
+        editShare=findViewById(R.id.share);
+
+        btnWebsite= findViewById(R.id.openWebsite);
+        btnLocation=findViewById(R.id.findLoaction);
+        btnShare=findViewById(R.id.shareButton);
+
+        btnWebsite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String text=editWebsite.getText().toString();
+                Uri webPage=Uri.parse(text);
+                Intent intent = new Intent(Intent.ACTION_VIEW,webPage);
+
+                if(intent.resolveActivity(getPackageManager())!=null){
+                    startActivity(intent);
+                }else {
+                    Log.e(TAG,"Can't handle Intent");
+                }
+            }
+        });
+
+        /*how to use Location Map in android*/
+
+        btnLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String loc=editlocation.getText().toString();
+
+                Uri addressUri=Uri.parse("geo:0,0?q="+loc);
+                Intent intent= new Intent(Intent.ACTION_VIEW,addressUri);
+                if(intent.resolveActivity(getPackageManager())!=null){
+                    startActivity(intent);
+                }else{
+                    Log.e(TAG,"Can't handle Intent");
+                }
+            }
+        });
+
+        /* Sharing text data to ANyone*/
+        btnShare.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String text=editShare.getText().toString();
+                String mimeType="text/plain";
+                ShareCompat.IntentBuilder
+                        .from(MainActivity.this)
+                        .setType(mimeType)
+                        .setText(text)
+                        .setChooserTitle("Share This Text to Everyone")
+                        .startChooser();
+            }
+        });
+    }
+}
