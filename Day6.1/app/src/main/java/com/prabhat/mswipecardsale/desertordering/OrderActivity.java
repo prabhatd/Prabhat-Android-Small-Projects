7fb565ad57b5060f3ac8b7ee1e45fa21030e0c51
Prabhat Dwivedi <prabhatd@pune-com-dt-014.payso.in>
2019-09-14 18:31:33 +0530

    Some changes IN order Activity and also click event handle of Radio button

--- Day6.1/app/src/main/java/com/prabhat/mswipecardsale/desertordering/OrderActivity.java
+++ Day6.1/app/src/main/java/com/prabhat/mswipecardsale/desertordering/OrderActivity.java
@@ -1,18 +1,50 @@
 package com.prabhat.mswipecardsale.desertordering;
 
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class OrderActivity extends AppCompatActivity {
     TextView txtMsg;
-
+    RadioGroup radioGroup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order);
         txtMsg= findViewById(R.id.txtOrder);
+        radioGroup=findViewById(R.id.radioGroup);
         String myOrder=getIntent().getStringExtra("orderMsg");
         txtMsg.setText(myOrder);
+    }
+
+    public void onRadioButtonClicked(View view){
+        boolean isChecked=((RadioButton)view).isChecked();
+
+        switch (view.getId()){
+            case R.id.radioSame:
+                if(isChecked)
+                showToast("You clicked Same Day Button");
+                break;
+            case R.id.radioNext:
+                if (isChecked)
+                showToast("You clicked Next Day Button");
+                break;
+            case R.id.radioPickup:
+                if(isChecked)
+                showToast("You clicked PickUp Button");
+                break;
+
+                default:
+                    break;
+        }
+    }
+
+    public void showToast(String message){
+        Toast.makeText(getApplicationContext(),message,Toast.LENGTH_LONG).show();
     }
 }
