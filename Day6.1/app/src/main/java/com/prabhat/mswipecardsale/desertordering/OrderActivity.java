c06703b73fe6acc04b46873291691f709b108a76
Prabhat Dwivedi <prabhatd@pune-com-dt-014.payso.in>
2019-09-14 18:52:03 +0530

    How does Spinner work in android Spinner addition

--- Day6.1/app/src/main/java/com/prabhat/mswipecardsale/desertordering/OrderActivity.java
+++ Day6.1/app/src/main/java/com/prabhat/mswipecardsale/desertordering/OrderActivity.java
@@ -4,12 +4,15 @@ import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class OrderActivity extends AppCompatActivity {
+public class OrderActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     TextView txtMsg;
     RadioGroup radioGroup;
     @Override
@@ -20,7 +23,21 @@ public class OrderActivity extends AppCompatActivity {
         radioGroup=findViewById(R.id.radioGroup);
         String myOrder=getIntent().getStringExtra("orderMsg");
         txtMsg.setText(myOrder);
+
+        Spinner spinner = findViewById(R.id.label_spinner);
+        if(spinner!=null){
+            spinner.setOnItemSelectedListener(this);
+        }
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.labels_array,android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        if(spinner!=null){
+            spinner.setAdapter(adapter);
+        }
     }
+
+
 
     public void onRadioButtonClicked(View view){
         boolean isChecked=((RadioButton)view).isChecked();
@@ -46,5 +63,16 @@ public class OrderActivity extends AppCompatActivity {
 
     public void showToast(String message){
         Toast.makeText(getApplicationContext(),message,Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+            String spinnerlable=parent.getItemAtPosition(position).toString();
+            showToast(spinnerlable);
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
