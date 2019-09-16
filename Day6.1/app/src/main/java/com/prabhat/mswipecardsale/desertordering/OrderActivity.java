8115d363e79749e94dcc4e22b334730bed709362
Prabhat Dwivedi <prabhatd@pune-com-dt-014.payso.in>
2019-09-16 15:06:10 +0530

    Open Phone Calling Activity after entering number inside edit text

--- Day6.1/app/src/main/java/com/prabhat/mswipecardsale/desertordering/OrderActivity.java
+++ Day6.1/app/src/main/java/com/prabhat/mswipecardsale/desertordering/OrderActivity.java
@@ -1,11 +1,17 @@
 package com.prabhat.mswipecardsale.desertordering;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
@@ -15,6 +21,8 @@ import android.widget.Toast;
 public class OrderActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     TextView txtMsg;
     RadioGroup radioGroup;
+    EditText editPhone;
+    public static final String TAG= OrderActivity.class.getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,7 +31,7 @@ public class OrderActivity extends AppCompatActivity implements AdapterView.OnIt
         radioGroup=findViewById(R.id.radioGroup);
         String myOrder=getIntent().getStringExtra("orderMsg");
         txtMsg.setText(myOrder);
-
+        editPhone=findViewById(R.id.editText_main);
         Spinner spinner = findViewById(R.id.label_spinner);
         if(spinner!=null){
             spinner.setOnItemSelectedListener(this);
@@ -35,6 +43,39 @@ public class OrderActivity extends AppCompatActivity implements AdapterView.OnIt
         if(spinner!=null){
             spinner.setAdapter(adapter);
         }
+
+        if(editPhone!=null){
+            editPhone.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+                @Override
+                public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                    boolean handled=false;
+                    if(actionId==EditorInfo.IME_ACTION_SEND){
+                        dialNumber();
+                        handled=true;
+                    }
+                    return handled;
+                }
+            });
+        }
+    }
+
+    private void dialNumber(){
+      EditText  editPhone=findViewById(R.id.editText_main);
+
+      String phoneNum=null;
+      if(editPhone!=null)phoneNum = "tel:"+editPhone.getText().toString();
+
+        Log.d(TAG,"dialNumber"+phoneNum);
+
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        intent.setData(Uri.parse(phoneNum));
+
+        if(intent.resolveActivity(getPackageManager())!=null){
+            startActivity(intent);
+        }else {
+            Log.d(TAG,"can't handle Intent");
+        }
+
     }
 
 
@@ -75,4 +116,8 @@ public class OrderActivity extends AppCompatActivity implements AdapterView.OnIt
     public void onNothingSelected(AdapterView<?> parent) {
 
     }
+
+   /* public void send(View v){
+
+    }*/
 }
