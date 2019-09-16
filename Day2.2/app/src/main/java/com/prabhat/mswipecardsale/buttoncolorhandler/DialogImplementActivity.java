bebd503c016c58c08d28b346e695061bb65872fc
Prabhat Dwivedi <prabhatd@pune-com-dt-014.payso.in>
2019-09-16 16:36:46 +0530

    How to use Date Picker Dialog Fragment inside Activity

---
+++ Day2.2/app/src/main/java/com/prabhat/mswipecardsale/buttoncolorhandler/DialogImplementActivity.java
@@ -0,0 +1,61 @@
+package com.prabhat.mswipecardsale.buttoncolorhandler;
+
+import android.content.DialogInterface;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class DialogImplementActivity extends AppCompatActivity {
+    Button btn1,btn2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dialog_implement);
+        btn1=findViewById(R.id.dialogButton);
+        btn2=findViewById(R.id.datePicker);
+    }
+
+    public void alert(View v){
+        AlertDialog.Builder builder= new AlertDialog.Builder(DialogImplementActivity.this);
+
+        builder.setTitle("Alert");
+        builder.setMessage("Click of to continue and Cancel to stop");
+
+        builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(getApplicationContext(), "Pressed OK",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(getApplicationContext(), "Pressed Cancel",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+        builder.show();
+    }
+
+    public void datePicker(View v){
+        DialogFragment dialogFragment= new DatePickerFragment();
+        dialogFragment.show(getSupportFragmentManager(),"datePicker");
+    }
+
+    public void processDatePickerResult(int year, int month, int dayOfMonth) {
+        String month_string = Integer.toString(month+1);
+        String day_string = Integer.toString(dayOfMonth);
+        String year_string = Integer.toString(year);
+        String dateMessage = (month_string +
+                "/" + day_string + "/" + year_string);
+
+        Toast.makeText(this, "Date: " + dateMessage,
+                Toast.LENGTH_SHORT).show();
+    }
+}
--- Day2.2/app/src/main/java/com/prabhat/mswipecardsale/buttoncolorhandler/MainActivity.java
+++ Day2.2/app/src/main/java/com/prabhat/mswipecardsale/buttoncolorhandler/MainActivity.java
@@ -1,13 +1,61 @@
 package com.prabhat.mswipecardsale.buttoncolorhandler;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    TextView textArticle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textArticle = findViewById(R.id.article);
+        registerForContextMenu(textArticle);
+
+
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_context, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.edit:
+                showToast("Edit choice clicked.");
+                return true;
+
+            case R.id.share:
+                showToast("Share choice clicked.");
+                return true;
+
+            case R.id.delete:
+                showToast("Delete choice clicked.");
+                return true;
+
+            default:
+                return super.onContextItemSelected(item);
+        }
+    }
+
+    public void showToast(String message) {
+        Toast.makeText(MainActivity.this, message, Toast.LENGTH_LONG).show();
+    }
+    public void next(View v){
+        Intent intent = new Intent(MainActivity.this,DialogImplementActivity.class);
+        startActivity(intent);
     }
 }
